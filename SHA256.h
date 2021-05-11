a1c4d5651496d1a326491f8c557702c0878dee2a
H1er <mejorjesus.99@gmail.com>
2021-05-11 16:19:58 +0200

    initial commit

---
+++ SHA256.h
@@ -0,0 +1,29 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+#include "ap_int.h"
+
+typedef ap_uint<512> bits512;
+typedef ap_uint<32> bits32;
+typedef ap_uint<9> bits9;
+typedef ap_uint<6> bits6;
+typedef ap_uint<5> bits5;
+typedef ap_uint<4> bits4;
+
+void get_Wi(bits6 &round, bits32 &Wi, bits32 &firstsWi);
+
+bits32 sigma_0lower (bits32 x);
+
+bits32 sigma_1lower (bits32 x);
+
+bits32 sigma_0upper (bits32 x);
+
+bits32 sigma_1upper (bits32 x);
+
+bits32 rotr (bits32 x, bits5 shift);
+
+bits32 maj (bits32 x, bits32 y, bits32 z);
+
+bits32 decomposition (bits6 &round, bits32 &firstsWi, bits512 block);
+
+#endif
